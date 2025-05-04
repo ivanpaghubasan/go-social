d4bf14e93663bbd9632e3b55b78c81559e394193
Ivan Paghubasan <ivan.paghubasan@hoolisoftware.com>
2025-05-04 22:07:10 +0800

    Update query for getting user feed

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -1,12 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/ivanpaghubasan/go-social/internal/store"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(fq); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
 	ctx := r.Context()
 
-	feed, err := app.store.Posts.GetUserFeed(ctx, int64(2))
+	feed, err := app.store.Posts.GetUserFeed(ctx, int64(2), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
